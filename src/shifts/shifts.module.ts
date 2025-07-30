b3102621a3ffa425725b88bc85fbd09dd0332b73
moshik <moshik0527669169@gmail.com>
2025-07-30 12:38:40 +0300

    add shift.entity file

--- src/shifts/shifts.module.ts
+++ src/shifts/shifts.module.ts
@@ -2,6 +2,7 @@ import { Module } from '@nestjs/common';
 import { ShiftsController } from './shifts.controller';
 import { ShiftsService } from './shifts.service';
 
+
 @Module({
   controllers: [ShiftsController],
   providers: [ShiftsService]
