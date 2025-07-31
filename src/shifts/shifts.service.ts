750c0167b84b8eff7f6f243bf3a856973df3ddbf
moshik <moshik0527669169@gmail.com>
2025-07-31 10:59:52 +0300

    shifts

--- src/shifts/shifts.service.ts
+++ src/shifts/shifts.service.ts
@@ -1,4 +1,26 @@
 import { Injectable } from '@nestjs/common';
+import Shift from './shifts.entity';
 
 @Injectable()
-export class ShiftsService {}
+export class ShiftsService {
+    async addShift(body) {
+        const shift = new Shift()
+        shift.soldierName = body.soldierName
+        shift.startTime = body.startTime
+        shift.endTime = body.endTime
+        shift.location = body.location
+        return await shift.save()
+    }
+
+    async getAllShifts() {
+        return await Shift.find();
+    }
+
+    async getShiftById(soldierName: string) {
+        return await Shift.findOne({ where: { soldierName } });
+    }
+
+
+
+}
+
