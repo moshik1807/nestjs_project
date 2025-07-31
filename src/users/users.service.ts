4a4c45e6cd78d274c486127f6713b5f35db02f77
moshik <moshik0527669169@gmail.com>
2025-07-31 12:04:22 +0300

    add function in usersService

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -4,8 +4,8 @@ import User from './users.entity';
 
 @Injectable()
 export class UsersService {
-  async create(body){
-    const hashPassword = await bcrypt.hash(body.password,10)
+  async create(body) {
+    const hashPassword = await bcrypt.hash(body.password, 10)
     const user = new User()
     user.username = body.username
     user.password = hashPassword
@@ -13,16 +13,25 @@ export class UsersService {
     return await user.save()
   }
 
-  async checkUser(username:string,password:string){
-    const user = await User.findOne({where:{username}})
-    if(!user) return null
-    const checkPassword = await bcrypt.compare(password,user.password)
-    if(checkPassword){ return user}
-    else{return null}
-  } 
-  
-  async getAllUsers(){
-    return await User.find() 
+  async getUserById(id: number) {
+    return await User.findOne({ where: { id } });
   }
-  
+
+  async findByUsername(username: string) {
+    return await User.findOne({ where: { username } });
+  }
+
+
+  async checkUser(username: string, password: string) {
+    const user = await User.findOne({ where: { username } })
+    if (!user) return null
+    const checkPassword = await bcrypt.compare(password, user.password)
+    if (checkPassword) { return user }
+    else { return null }
+  }
+
+  async getAllUsers() {
+    return await User.find()
+  }
+
 }
