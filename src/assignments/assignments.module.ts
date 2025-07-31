07bdab6079903bdbc8a3a31cbc2c49039cb910c0
moshik <moshik0527669169@gmail.com>
2025-07-31 16:41:33 +0300

    add protected in assignmentsService

--- src/assignments/assignments.module.ts
+++ src/assignments/assignments.module.ts
@@ -1,11 +1,28 @@
+// import { Module } from '@nestjs/common';
+// import { TypeOrmModule } from '@nestjs/typeorm';
+// import Assignment from './assignments.entity';
+// import { AssignmentsService } from './assignments.service';
+// import { AssignmentsController } from './assignments.controller';
+
+// @Module({
+//   imports: [TypeOrmModule.forFeature([Assignment])],
+//   providers: [AssignmentsService],
+//   controllers: [AssignmentsController],
+//   exports: [AssignmentsService],
+// })
+// export class AssignmentsModule {}
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import Assignment from './assignments.entity';
 import { AssignmentsService } from './assignments.service';
 import { AssignmentsController } from './assignments.controller';
+import { AuthModule } from '../auth/auth.module';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Assignment])],
+  imports: [
+    TypeOrmModule.forFeature([Assignment]),
+    AuthModule  // מייבא את AuthModule כדי לגשת ל-AuthGuard ו-RolesGuard
+  ],
   providers: [AssignmentsService],
   controllers: [AssignmentsController],
   exports: [AssignmentsService],
---
+++ src/auth/roles.enum.ts
@@ -0,0 +1,4 @@
+export enum Role {
+  COMMANDER = 'commander',
+  SOLDIER = 'soldier'
+}
